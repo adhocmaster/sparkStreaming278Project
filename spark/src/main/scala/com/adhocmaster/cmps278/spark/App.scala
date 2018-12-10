3a66ccf29df5f55577511d9801f6bb48a9d230db
Adhocmaster <adhocmaster@live.com>
2018-12-10 15:35:17 -0800

    setting up streaming requirements.

--- spark/src/main/scala/com/adhocmaster/cmps278/spark/App.scala
+++ spark/src/main/scala/com/adhocmaster/cmps278/spark/App.scala
@@ -4,6 +4,8 @@ import org.apache.spark.sql.SparkSession
 import com.adhocmaster.cmps278.spark.util.ConfigurationManager
 import com.adhocmaster.cmps278.spark.data.BabyNames
 import org.apache.log4j.Logger
+import org.apache.spark._
+import org.apache.spark.streaming._
 
 /**
  * @author ${user.name}
@@ -11,6 +13,9 @@ import org.apache.log4j.Logger
 object App {
 
   val logger = Logger.getLogger( getClass.getName )
+  var spark = null
+  var sc: SparkContext = null
+  var ssc: StreamingContext = null
 
   def main( args: Array[String] ) {
 
@@ -28,7 +33,8 @@ object App {
       .master( "local[*]" )
       .getOrCreate()
 
-    val sc = spark.sparkContext
+    sc = spark.sparkContext
+    ssc = new StreamingContext( sc, Milliseconds( ConfigurationManager.getVal( "streaming.intervalInMiliseconds" ).get.toLong ) )
 
     BabyNames.loadAsDF( spark )
 
