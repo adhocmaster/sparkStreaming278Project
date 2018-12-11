51eee83fa85fc76ab95542286240eda65c66877d
Adhocmaster <adhocmaster@live.com>
2018-12-10 22:58:11 -0800

    started structured streaming

---
+++ spark/src/main/scala/com/adhocmaster/cmps278/spark/structuredStreaming/SSApp.scala
@@ -0,0 +1,14 @@
+package com.adhocmaster.cmps278.spark.structuredStreaming
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkContext
+import org.apache.spark.streaming.StreamingContext
+
+class SSApp( spark: SparkSession, sc: SparkContext, ssc: StreamingContext, inputDir: String ) {
+
+  def run = {
+
+    val fileStream = ssc.textFileStream( inputDir )
+
+  }
+}
